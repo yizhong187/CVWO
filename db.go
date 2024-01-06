80ddee931e862b8e65b6498cbc454fc11a88c373
yizhong187 <yizhong187@gmail.com>
2024-01-06 12:53:51 +0800

    initial commit

---
+++ db.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+var db *sql.DB
+
+// initDB initializes the database connection
+func initDB() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbSslmode := os.Getenv("DB_SSLMODE")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
+		dbHost, dbPort, dbUser, dbName, dbSslmode, dbPassword)
+
+	db, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+}
