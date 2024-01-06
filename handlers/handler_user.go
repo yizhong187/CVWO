425bca267825c5dd6c7a6c2fd25831995607806b
yizhong187 <yizhong187@gmail.com>
2024-01-07 01:33:17 +0800

    updated user model implementation

--- handlers/handler_user.go
+++ handlers/handler_user.go
@@ -16,17 +16,17 @@ func HandlerUser(w http.ResponseWriter, r *http.Request) {
 
 	godotenv.Load(".env")
 
-	userDataTable := os.Getenv("DB_USERDATA_TABLE")
-	query := fmt.Sprintf("SELECT id, name FROM %s WHERE id = $1", userDataTable)
+	usersTable := os.Getenv("DB_USERS_TABLE")
+	query := fmt.Sprintf("SELECT * FROM %s WHERE username = $1", usersTable)
 
-	userID := r.URL.Query().Get("id")
-	if userID == "" {
-		util.RespondWithError(w, http.StatusBadRequest, "User ID is required")
+	userName := r.URL.Query().Get("name")
+	if userName == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "Username is required")
 		return
 	}
 
 	var user models.User
-	err := database.GetDB().QueryRow(query, userID).Scan(&user.ID, &user.Name)
+	err := database.GetDB().QueryRow(query, userName).Scan(&user.Name, &user.UserType)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			util.RespondWithError(w, http.StatusNotFound, "User not found")
