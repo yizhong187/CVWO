88b2713f2d5b21368c3ee2f702f8bbde165acfb8
yizhong187 <yizhong187@gmail.com>
2024-01-07 20:34:01 +0800

    testing a different function

--- handlers/handler_testing.go
+++ handlers/handler_testing.go
@@ -4,32 +4,39 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 	"github.com/yizhong187/CVWO/database"
+	"github.com/yizhong187/CVWO/models"
 	"github.com/yizhong187/CVWO/util"
 )
 
 func HandlerTest(w http.ResponseWriter, r *http.Request) {
+	godotenv.Load(".env")
 
-	userID := "1"
+	usersTable := os.Getenv("DB_USERS_TABLE")
+	query := fmt.Sprintf("SELECT * FROM %s WHERE name = $1", usersTable)
 
-	type TestingUser struct {
-		ID       int    `json:"id"`
-		Username string `json:"username"`
+	// Extracting the name from the URL path
+	userName := chi.URLParam(r, "name")
+	if userName == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "User's name is required")
+		return
 	}
 
-	var testingUser TestingUser
-
-	err := database.GetDB().QueryRow("SELECT * FROM public.testing WHERE id = $1", userID).Scan(&testingUser.ID, &testingUser.Username)
+	var user models.User
+	err := database.GetDB().QueryRow(query, userName).Scan(&user.ID, &user.Name, &user.Type, &user.CreatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			util.RespondWithError(w, http.StatusNotFound, "User not found")
 		} else {
-			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: %v", err))
+			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: \n%v", err))
 		}
 		return
 	}
 
 	// Respond with the user data in JSON format
-	util.RespondWithJSON(w, http.StatusOK, testingUser)
+	util.RespondWithJSON(w, http.StatusOK, user)
 }
