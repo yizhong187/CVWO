3a32d27002c61f27544542d12205c55b13473192
yizhong187 <yizhong187@gmail.com>
2024-01-07 20:33:15 +0800

    renamed router.go to user_router.go

---
+++ routers/user_router.go
@@ -0,0 +1,26 @@
+package routers
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/yizhong187/CVWO/handlers"
+)
+
+func UserRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/users", func(r chi.Router) {
+		r.Get("/healthz", handlers.HandlerReadiness)
+		r.Get("/err", handlers.HandlerErr)
+		r.Post("/", handlers.HandlerCreateUser)
+		r.Get("/", handlers.HandlerAllUsers)
+		r.Get("/{name}", handlers.HandlerTest)
+		r.Put("/{name}", handlers.HandlerUpdateUser)
+		//r.Delete("/{name}", DeleteUser)
+	})
+
+	return r
+}
