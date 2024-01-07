c2a98d60b28e7e1f29c8a005097bc864422d5ac7
yizhong187 <yizhong187@gmail.com>
2024-01-07 22:10:28 +0800

    initial commit

---
+++ handlers/handler_delete_user.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"github.com/yizhong187/CVWO/database"
+	"github.com/yizhong187/CVWO/util"
+)
+
+func HandlerDeleteUser(w http.ResponseWriter, r *http.Request) {
+	godotenv.Load(".env")
+	usersTable := os.Getenv("DB_USERS_TABLE")
+	if usersTable == "" {
+		log.Fatal("usersTable is not set in the environment")
+	}
+
+	// Retrieve user name from URL path parameter
+	userName := chi.URLParam(r, "name")
+	if userName == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "User's name is required")
+		return
+	}
+
+	// Delete the user from the database
+	query := fmt.Sprintf("DELETE FROM %s WHERE name = $1", usersTable)
+	result, err := database.GetDB().Exec(query, userName)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: \n%v", err))
+		return
+	}
+
+	// Check if a row was actually deleted
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error checking rows affected: \n%v", err))
+		return
+	}
+	if rowsAffected == 0 {
+		util.RespondWithError(w, http.StatusNotFound, "User not found or already deleted")
+		return
+	}
+
+	// Respond with a success message
+	util.RespondWithJSON(w, http.StatusOK, map[string]string{"message": "User successfully deleted"})
+}
