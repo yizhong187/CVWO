0f1259c8223ea79713c62221fc96a7c8c6b703fe
yizhong187 <yizhong187@gmail.com>
2024-01-07 20:33:28 +0800

    added v2Router to testing

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"github.com/yizhong187/CVWO/database"
-	"github.com/yizhong187/CVWO/handlers"
+	"github.com/yizhong187/CVWO/routers"
 )
 
 func main() {
@@ -25,15 +25,16 @@ func main() {
 
 	router := chi.NewRouter()
 
-	v1Router := chi.NewRouter()
+	// v1Router := chi.NewRouter()
+	// v1Router.Get("/healthz", handlers.HandlerReadiness)
+	// v1Router.Get("/err", handlers.HandlerErr)
+	// v1Router.Get("/user", handlers.HandlerUser)
+	// v1Router.Post("/createuser", handlers.HandlerCreateUser)
+	// v1Router.Get("/testing", handlers.HandlerTest)
+	// router.Mount("/v1", v1Router)
 
-	v1Router.Get("/healthz", handlers.HandlerReadiness)
-	v1Router.Get("/err", handlers.HandlerErr)
-	v1Router.Get("/user", handlers.HandlerUser)
-	v1Router.Post("/createuser", handlers.HandlerCreateUser)
-	v1Router.Get("/testing", handlers.HandlerTest)
-
-	router.Mount("/v1", v1Router)
+	v2Router := routers.UserRouter()
+	router.Mount("/v2", v2Router)
 
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", router)
