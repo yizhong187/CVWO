f654ef1d4e962ed27bcffc7595be8873fb9e9e25
yizhong187 <yizhong187@gmail.com>
2024-01-07 22:12:34 +0800

    implemented delete route

--- routers/user_router.go
+++ routers/user_router.go
@@ -2,15 +2,15 @@ package routers
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	//"github.com/go-chi/chi/v5/middleware"
 	"github.com/yizhong187/CVWO/handlers"
 )
 
 func UserRouter() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+	//r.Use(middleware.Logger)
+	//r.Use(middleware.Recoverer)
 
 	r.Route("/users", func(r chi.Router) {
 		r.Get("/healthz", handlers.HandlerReadiness)
@@ -19,7 +19,7 @@ func UserRouter() *chi.Mux {
 		r.Get("/", handlers.HandlerAllUsers)
 		r.Get("/{name}", handlers.HandlerTest)
 		r.Put("/{name}", handlers.HandlerUpdateUser)
-		//r.Delete("/{name}", DeleteUser)
+		r.Delete("/{name}", handlers.HandlerDeleteUser)
 	})
 
 	return r
