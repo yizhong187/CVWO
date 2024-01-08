2b2d61c557eee9664e960ac9407c6e4075fe9056
yizhong187 <yizhong187@gmail.com>
2024-01-08 16:36:28 +0800

    abstracted using util.QueryUser

--- handlers/handler_user.go
+++ handlers/handler_user.go
@@ -1,44 +1,26 @@
 package handlers
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
-	"github.com/yizhong187/CVWO/database"
-	"github.com/yizhong187/CVWO/models"
 	"github.com/yizhong187/CVWO/util"
 )
 
 func HandlerUser(w http.ResponseWriter, r *http.Request) {
 
-	godotenv.Load(".env")
-	usersTable := os.Getenv("DB_USERS_TABLE")
-	if usersTable == "" {
-		log.Fatal("usersTable is not set in the environment")
-	}
-
-	// Retrieve user name from URL query
 	userName := chi.URLParam(r, "name")
 	if userName == "" {
 		util.RespondWithError(w, http.StatusBadRequest, "User's name is required")
 		return
 	}
 
-	var user models.User
-
-	// Query the database for the user
-	query := fmt.Sprintf("SELECT * FROM %s WHERE name = $1", usersTable)
-	err := database.GetDB().QueryRow(query, userName).Scan(&user.ID, &user.Name, &user.Type, &user.CreatedAt)
+	user, err := util.QueryUser(userName)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			util.RespondWithError(w, http.StatusNotFound, "User not found")
+		if err.Error() == "User not found!" {
+			util.RespondWithError(w, http.StatusNotFound, err.Error())
 		} else {
-			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: \n%v", err))
+			util.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		}
 		return
 	}
