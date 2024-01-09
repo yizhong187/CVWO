591bf6a3ae753c49425ba96ab097ae9c494d3e27
yizhong187 <yizhong187@gmail.com>
2024-01-09 17:23:40 +0800

    added post reply endpoint

--- routers/thread_router.go
+++ routers/thread_router.go
@@ -9,13 +9,12 @@ func ThreadRouter() *chi.Mux {
 	r := chi.NewRouter()
 
 	// Thread-related endpoints
-	r.Post("/", handlers.HandlerCreateThread) // Endpoint for creating a new thread
-	r.Get("/", handlers.HandlerAllThreads)    // Endpoint for retrieving all threads in a subforum
-	//r.Get("/{threadID}", handlers.HandlerThread)      // Endpoint for retrieving a specific thread
+	r.Post("/", handlers.HandlerCreateThread)    // Endpoint for creating a new thread
+	r.Get("/", handlers.HandlerAllThreads)       // Endpoint for retrieving all threads in a subforum
+	r.Get("/{threadID}", handlers.HandlerThread) // Endpoint for retrieving a specific thread, and its replies
 	//r.Put("/{threadID}", handlers.HandlerUpdateThread) // Endpoint for updating a specific thread
 	//r.Delete("/{threadID}", handlers.HandlerDeleteThread) // Endpoint for deleting a specific thread
 
-	// Additional thread-related endpoints can be added here
-
+	r.Post("/{threadID}", handlers.HandlerPostReply) // Endpoint for posting a new reply in a thread
 	return r
 }
