a0bed71075b142c2efbc1c2a26d6f98a66882169
yizhong187 <yizhong187@gmail.com>
2024-01-14 23:34:22 +0800

    updated to extract id from query string instead of request body

--- handlers/handler_userid_to_username.go
+++ handlers/handler_userid_to_username.go
@@ -1,42 +1,21 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/yizhong187/CVWO/util"
 )
 
 func HandlerUseridToUsername(w http.ResponseWriter, r *http.Request) {
 
-	godotenv.Load(".env")
-	repliesTable := os.Getenv("DB_USERS_TABLE")
-	if repliesTable == "" {
-		util.RespondWithError(w, http.StatusInternalServerError, "DB_USERS_TABLE is not set in the environment")
-		return
-	}
-
-	// Decode the JSON request body into CreateUsernameRequest struct
-	type CreateUsernameRequestData struct {
-		ID string `json:"id"`
-	}
-	var requestData CreateUsernameRequestData
-	err := json.NewDecoder(r.Body).Decode(&requestData)
-	if err != nil {
-		util.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	defer r.Body.Close()
-
-	// Check for empty content
-	if requestData.ID == "" {
+	// Extract the ID from the query string
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
 		util.RespondWithError(w, http.StatusBadRequest, "ID is required")
 		return
 	}
 
-	username, err := util.QueryUsername(requestData.ID)
+	username, err := util.QueryUsername(userID)
 	if err != nil {
 		if err.Error() == "User not found!" {
 			util.RespondWithError(w, http.StatusNotFound, err.Error())
