8296aaedef4ea190bb826fcd08741e980f399876
yizhong187 <yizhong187@gmail.com>
2024-01-16 15:54:21 +0800

    removed v1 router

--- main.go
+++ main.go
@@ -34,26 +34,20 @@ func main() {
 		MaxAge:           300,
 	}))
 
-	// v1Router := chi.NewRouter()
-	// v1Router.Get("/healthz", handlers.HandlerReadiness)
-	// v1Router.Get("/err", handlers.HandlerErr)
-	// v1Router.Get("/user", handlers.HandlerUser)
-	// v1Router.Post("/createuser", handlers.HandlerCreateUser)
-	// v1Router.Get("/testing", handlers.HandlerTest)
-	// router.Mount("/v1", v1Router)
-
-	// v2UserRouter := routers.UserRouter()
-	// router.Mount("/v2", v2UserRouter)
-
 	// Version 2 API routes
 	v2Router := chi.NewRouter()
+
+	v2Router.Get("/healthz", handlers.HandlerReadiness)
+	v2Router.Get("/err", handlers.HandlerErr)
+
+	v2Router.Post("/signup", handlers.HandlerSignup)
+	v2Router.Post("/login", handlers.HandlerLogin)
+	v2Router.Get("/logout", handlers.HandlerLogout)
+	v2Router.Get("/user", handlers.HandlerTesting)
+
 	v2Router.Mount("/users", routers.UserRouter())
 	v2Router.Get("/healthz", handlers.HandlerReadiness)
 	v2Router.Get("/err", handlers.HandlerErr)
-	v2Router.Post("/register", handlers.HandlerRegister)
-	v2Router.Post("/login", handlers.HandlerLogin)
-	v2Router.Get("/user", handlers.HandlerTesting)
-	v2Router.Get("/logout", handlers.HandlerLogout)
 
 	// Mount v2Router under /v2 prefix
 	router.Mount("/v2", v2Router)
