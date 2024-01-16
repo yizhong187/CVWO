545d210dbdb09a8dee9033f5a7362af7e7618426
yizhong187 <yizhong187@gmail.com>
2024-01-16 19:51:04 +0800

    testing

--- handlers/handler_testing.go
+++ handlers/handler_testing.go
@@ -1,13 +1,12 @@
 package handlers
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"github.com/yizhong187/CVWO/database"
 	"github.com/yizhong187/CVWO/models"
@@ -17,57 +16,69 @@ import (
 func HandlerTesting(w http.ResponseWriter, r *http.Request) {
 
 	godotenv.Load(".env")
-	usersTable := os.Getenv("DB_USERS_TABLE")
-	if usersTable == "" {
-		log.Fatal("usersTable is not set in the environment")
-	}
-	secretKey := os.Getenv("SECRET_KEY")
-	if usersTable == "" {
-		log.Fatal("SECRET_KEY is not set in the environment")
-	}
+	threadsTable := os.Getenv("DB_THREADS_TABLE")
+	repliesTable := os.Getenv("DB_REPLIES_TABLE")
 
-	cookie, err := r.Cookie("jwt")
+	// Retrieve threadID from URL parameter
+	threadID := chi.URLParam(r, "threadID")
+
+	// First SQL query to get the thread
+	threadQuery := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", threadsTable)
+	row := database.GetDB().QueryRow(threadQuery, threadID)
+	var thread models.Thread
+	err := row.Scan(&thread.ID, &thread.SubforumID, &thread.Title, &thread.Content, &thread.CreatedBy, &thread.IsPinned, &thread.CreatedAt, &thread.UpdatedAt)
 	if err != nil {
-		util.RespondWithError(w, http.StatusUnauthorized, "User unauthenticated")
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving thread: \n%v", err))
 		return
 	}
 
-	token, err := jwt.ParseWithClaims(cookie.Value, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(secretKey), nil
-	})
-
+	// Second SQL query to get replies associated with the thread
+	repliesQuery := fmt.Sprintf("SELECT * FROM %s WHERE thread_id = $1", repliesTable)
+	rows, err := database.GetDB().Query(repliesQuery, threadID)
 	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			util.RespondWithError(w, http.StatusUnauthorized, "User unauthenticated")
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving replies: \n%v", err))
+		return
+	}
+	defer rows.Close()
+
+	var replies []models.Reply
+	for rows.Next() {
+		var reply models.Reply
+		err := rows.Scan(&reply.ID, &reply.ThreadID, &reply.Content, &reply.CreatedBy, &reply.CreatedAt, &reply.UpdatedAt)
+		if err != nil {
+			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error scanning row: \n%v", err))
 			return
 		}
-		util.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Bad request: \n%v", err))
+		replies = append(replies, reply)
+	}
+
+	if err = rows.Err(); err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Row error: \n%v", err))
 		return
 	}
 
-	if !token.Valid {
-		util.RespondWithError(w, http.StatusUnauthorized, "User unauthenticated")
-		return
-	}
-
-	claims, ok := token.Claims.(*jwt.RegisteredClaims)
-	if !ok {
-		util.RespondWithError(w, http.StatusUnauthorized, "User unauthenticated")
-		return
-	}
-
-	var user models.TestingUser
-	query := fmt.Sprintf("SELECT id, name, type, created_at FROM %s WHERE id = $1", usersTable)
-	err = database.GetDB().QueryRow(query, claims.Subject).Scan(&user.ID, &user.Name, &user.Type, &user.CreatedAt)
+	// Third SQL query to get replyCount for the thread
+	numThreadID, err := strconv.Atoi(threadID)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			util.RespondWithError(w, http.StatusBadRequest, "User not found")
-			return
-		} else {
-			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving user type: %v", err))
-			return
-		}
+		util.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Invalid Parameter: \n%v", err))
+		return
 	}
 
-	util.RespondWithJSON(w, http.StatusOK, user)
+	replyCount, err := util.QueryReplyCount(numThreadID)
+	if err != nil {
+		util.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error retrieving replycount: \n%v", err))
+		return
+	}
+
+	// Respond with thread and its replies using the RespondWithJSON function
+	response := struct {
+		Thread     models.Thread  `json:"thread"`
+		Replies    []models.Reply `json:"replies"`
+		ReplyCount int            `json:"replyCount"`
+	}{
+		Thread:     thread,
+		Replies:    replies,
+		ReplyCount: replyCount,
+	}
+	util.RespondWithJSON(w, http.StatusOK, response)
 }
