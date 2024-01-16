b41d93a57f25af92024c76accc044a1cfeb32b5d
yizhong187 <yizhong187@gmail.com>
2024-01-16 19:51:52 +0800

    changed formating of JSON response for thread

--- handlers/handler_thread.go
+++ handlers/handler_thread.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
@@ -16,7 +17,6 @@ import (
 func HandlerThread(w http.ResponseWriter, r *http.Request) {
 	godotenv.Load(".env")
 	threadsTable := os.Getenv("DB_THREADS_TABLE")
-	repliesTable := os.Getenv("DB_REPLIES_TABLE")
 
 	// Retrieve threadID from URL parameter
 	threadID := chi.URLParam(r, "threadID")
@@ -31,32 +31,7 @@ func HandlerThread(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Second SQL query to get replies associated with the thread
-	repliesQuery := fmt.Sprintf("SELECT * FROM %s WHERE thread_id = $1", repliesTable)
-	rows, err := database.GetDB().Query(repliesQuery, threadID)
-	if err != nil {
-		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving replies: \n%v", err))
-		return
-	}
-	defer rows.Close()
-
-	var replies []models.Reply
-	for rows.Next() {
-		var reply models.Reply
-		err := rows.Scan(&reply.ID, &reply.ThreadID, &reply.Content, &reply.CreatedBy, &reply.CreatedAt, &reply.UpdatedAt)
-		if err != nil {
-			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error scanning row: \n%v", err))
-			return
-		}
-		replies = append(replies, reply)
-	}
-
-	if err = rows.Err(); err != nil {
-		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Row error: \n%v", err))
-		return
-	}
-
-	// Third SQL query to get replyCount for the thread
+	// Second SQL query to get replyCount for the thread
 	numThreadID, err := strconv.Atoi(threadID)
 	if err != nil {
 		util.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Invalid Parameter: \n%v", err))
@@ -71,12 +46,24 @@ func HandlerThread(w http.ResponseWriter, r *http.Request) {
 
 	// Respond with thread and its replies using the RespondWithJSON function
 	response := struct {
-		Thread     models.Thread  `json:"thread"`
-		Replies    []models.Reply `json:"replies"`
-		ReplyCount int            `json:"replyCount"`
+		ID         int       `json:"id"`
+		SubforumID int       `json:"subforumID"`
+		Title      string    `json:"title"`
+		Content    string    `json:"content"`
+		CreatedBy  string    `json:"createdBy"`
+		IsPinned   bool      `json:"isPinned"`
+		CreatedAt  time.Time `json:"createdAt"`
+		UpdatedAt  time.Time `json:"updatedAt"`
+		ReplyCount int       `json:"replyCount"`
 	}{
-		Thread:     thread,
-		Replies:    replies,
+		ID:         thread.ID,
+		SubforumID: thread.SubforumID,
+		Title:      thread.Title,
+		Content:    thread.Content,
+		CreatedBy:  thread.CreatedBy,
+		IsPinned:   thread.IsPinned,
+		CreatedAt:  thread.CreatedAt,
+		UpdatedAt:  thread.UpdatedAt,
 		ReplyCount: replyCount,
 	}
 	util.RespondWithJSON(w, http.StatusOK, response)
