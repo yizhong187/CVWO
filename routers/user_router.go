b05ae204a8b57f6ea3f048e7a7a71efa66b5bea0
yizhong187 <yizhong187@gmail.com>
2024-01-21 21:16:31 +0800

    undeprecated and added new endpoints

--- routers/user_router.go
+++ routers/user_router.go
@@ -1,35 +1,25 @@
-//[DEPRECATED]
-
 package routers
 
 import (
 	"github.com/go-chi/chi/v5"
-	//"github.com/go-chi/chi/v5/middleware"
 	"github.com/yizhong187/CVWO/handlers"
+	"github.com/yizhong187/CVWO/util"
 )
 
 func UserRouter() *chi.Mux {
 	r := chi.NewRouter()
 
-	//r.Use(middleware.Logger)
-	//r.Use(middleware.Recoverer)
-
 	r.Route("/", func(r chi.Router) {
 		r.Get("/healthz", handlers.HandlerReadiness) // Health check endpoint to verify the service status
 		r.Get("/err", handlers.HandlerErr)           // Endpoint to test error handling
 
-		// User-related endpoints for creating, retrieving, updating, and deleting users
-		//r.Post("/", handlers.HandlerCreateUser)
-		r.Get("/", handlers.HandlerAllUsers)
-		r.Get("/username", handlers.HandlerUseridToUsername)
-		r.Put("/{name}", handlers.HandlerUpdateUser)
-		r.Delete("/{name}", handlers.HandlerDeleteUser)
+		// User-related endpoints for retrieving and updating user information.
+		r.With(util.AuthenticateUserMiddleware).Get("/", handlers.HandlerUser)
+		r.With(util.AuthenticateUserMiddleware).Put("/", handlers.HandlerUpdateUser)
 
-		// Superuser-related endpoints for editting user types
-		r.Put("/{name}", handlers.HandlerUpdateUserType)
+		// For retrieving all posts (threads and replies) by existing user.
+		r.Get("/{userName}/posts", handlers.HandlerUserPosts)
 
-		// Mount SubforumRouter under a specific user
-		r.Mount("/{name}/subforum", SubforumRouter())
 	})
 
 	return r
