14338e8578713b97acbd8428159c5d23d61ae0d6
yizhong187 <yizhong187@gmail.com>
2024-01-21 21:19:09 +0800

    remove check for user when accessing userposts, so now all user posts are public

--- handlers/handler_user_posts.go
+++ handlers/handler_user_posts.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"github.com/yizhong187/CVWO/database"
 	"github.com/yizhong187/CVWO/models"
@@ -28,38 +28,43 @@ func HandlerUserPosts(w http.ResponseWriter, r *http.Request) {
 	if usersTable == "" {
 		log.Fatal("repliesTable is not set in the environment")
 	}
+	subforumsTable := os.Getenv("DB_SUBFORUMS_TABLE")
+	if subforumsTable == "" {
+		log.Fatal("subforumTable is not set in the environment")
+	}
 
-	// Retrieve the claims from the middleware context (util.AuthenticateUserMiddleware)
-	claims, ok := r.Context().Value("userClaims").(*jwt.RegisteredClaims)
-	if !ok {
-		util.RespondWithError(w, http.StatusInternalServerError, "Error processing user data")
+	// Retrieve username from URL query
+	userName := chi.URLParam(r, "userName")
+	if userName == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "userName is required")
 		return
 	}
 
 	// SQL query to get the user's threads
-	query := fmt.Sprintf(`
-	SELECT t.id, t.subforum_id, t.title, t.content, t.created_by, u.name, t.is_pinned, t.created_at, t.updated_at, COUNT(r.id) AS reply_count
+	threadQuery := fmt.Sprintf(`
+	SELECT t.id, t.subforum_id, sf.name AS subforum_name, t.title, t.content, t.created_by, u.name, t.is_pinned, t.created_at, t.updated_at, COUNT(r.id) AS reply_count
 	FROM %s t
 	INNER JOIN %s u ON t.created_by = u.id
 	LEFT JOIN %s r ON t.id = r.thread_id
-	WHERE t.created_by = $1
-	GROUP BY t.id, u.name
+	INNER JOIN %s sf ON t.subforum_id = sf.id
+	WHERE u.name = $1
+	GROUP BY t.id, u.name, sf.name
 	ORDER BY t.id DESC
-	`, threadsTable, usersTable, repliesTable)
+`, threadsTable, usersTable, repliesTable, subforumsTable)
 
-	// Execute sql query and return a rows result set
-	rows, err := database.GetDB().Query(query, claims.Subject)
+	// Execute sql query and return a rows result set for threads
+	threadRows, err := database.GetDB().Query(threadQuery, userName)
 	if err != nil {
 		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: \n%v", err))
 		return
 	}
-	defer rows.Close()
+	defer threadRows.Close()
 
 	// Initialize a slice of threads, scan each row into a thread struct and append into the slice
 	var threads []models.Thread
-	for rows.Next() {
+	for threadRows.Next() {
 		var thread models.Thread
-		err := rows.Scan(&thread.ID, &thread.SubforumID, &thread.Title, &thread.Content, &thread.CreatedBy,
+		err := threadRows.Scan(&thread.ID, &thread.SubforumID, &thread.SubforumName, &thread.Title, &thread.Content, &thread.CreatedBy,
 			&thread.CreatedByName, &thread.IsPinned, &thread.CreatedAt, &thread.UpdatedAt, &thread.ReplyCount)
 		if err != nil {
 			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error scanning row: \n%v", err))
@@ -68,33 +73,35 @@ func HandlerUserPosts(w http.ResponseWriter, r *http.Request) {
 		threads = append(threads, thread)
 	}
 
-	if err = rows.Err(); err != nil {
+	if err = threadRows.Err(); err != nil {
 		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Row error: \n%v", err))
 		return
 	}
 
-	// Modify the query to join with users table to get createdByName
-	query = fmt.Sprintf(`
-        SELECT r.id, r.thread_id, r.content, r.created_by, u.name AS created_by_name, r.created_at, r.updated_at
-        FROM %s r
-        INNER JOIN %s u ON r.created_by = u.id
-        WHERE r.created_by = $1
-				ORDER BY r.id DESC
-    `, repliesTable, usersTable)
+	// SQL query to get thread and subforum info for replies
+	replyQuery := fmt.Sprintf(`
+	SELECT r.id, r.thread_id, t.title AS thread_name, t.subforum_id, sf.name AS subforum_name, r.content, r.created_by, u.name AS created_by_name, r.created_at, r.updated_at
+	FROM %s r
+	INNER JOIN %s u ON r.created_by = u.id
+	INNER JOIN %s t ON r.thread_id = t.id
+	INNER JOIN %s sf ON t.subforum_id = sf.id
+	WHERE u.name = $1
+	ORDER BY r.id DESC
+`, repliesTable, usersTable, threadsTable, subforumsTable)
 
-	// Execute SQL query and return a rows result set
-	rows, err = database.GetDB().Query(query, claims.Subject)
+	// Execute SQL query and return a rows result set for replies
+	replyRows, err := database.GetDB().Query(replyQuery, userName)
 	if err != nil {
 		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Internal Server Error: \n%v", err))
 		return
 	}
-	defer rows.Close()
+	defer replyRows.Close()
 
 	// Initialize a slice of replies, scan each row into a reply struct, and append it to the slice
 	var replies []models.Reply
-	for rows.Next() {
+	for replyRows.Next() {
 		var reply models.Reply
-		err := rows.Scan(&reply.ID, &reply.ThreadID, &reply.Content, &reply.CreatedBy, &reply.CreatedByName, &reply.CreatedAt, &reply.UpdatedAt)
+		err := replyRows.Scan(&reply.ID, &reply.ThreadID, &reply.ThreadName, &reply.SubforumID, &reply.SubforumName, &reply.Content, &reply.CreatedBy, &reply.CreatedByName, &reply.CreatedAt, &reply.UpdatedAt)
 		if err != nil {
 			util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error scanning row: \n%v", err))
 			return
@@ -102,16 +109,13 @@ func HandlerUserPosts(w http.ResponseWriter, r *http.Request) {
 		replies = append(replies, reply)
 	}
 
-	if err = rows.Err(); err != nil {
+	if err = replyRows.Err(); err != nil {
 		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Row error: \n%v", err))
 		return
 	}
 
-	log.Println(threads)
-	log.Println(replies)
-
 	util.RespondWithJSON(w, http.StatusOK, struct {
-		Threads []models.Thread
-		Replies []models.Reply
+		Threads []models.Thread `json:"threads"`
+		Replies []models.Reply  `json:"replies"`
 	}{Threads: threads, Replies: replies})
 }
